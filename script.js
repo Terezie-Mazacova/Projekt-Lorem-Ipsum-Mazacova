a5801823cd90d6a0b8c11e813631653962d1e0ad
Terezie Mazáčová <terezkamazacova@post.cz>
2025-04-15 23:17:36 +0200

    Header v beta verzi, založen script.js a components - _header.scss

---
+++ script.js
@@ -0,0 +1,8 @@
+// == HAMBURGER MENU ==
+const hamburger = document.querySelector('.hamburger');
+const mobileNav = document.querySelector('.mobile-nav');
+
+hamburger.addEventListener('click', function() {
+    this.classList.toggle('is-active');
+    mobileNav.classList.toggle('is-active');
+});
