167f58c57b9eeb1679d9c6e19c0276b57a8db3ec
Terezie Mazáčová <terezkamazacova@post.cz>
2025-04-18 10:34:26 +0200

    Snad už hotové menu - header

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 // == HAMBURGER MENU ==
 const hamburger = document.querySelector('.hamburger');
 const mobileNav = document.querySelector('.mobile-nav');
+const navLinks = document.querySelectorAll('.mobile-nav a');
 
-hamburger.addEventListener('click', function() {
+hamburger.addEventListener('click', function () {
     this.classList.toggle('is-active');
     mobileNav.classList.toggle('is-active');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+    hamburger.classList.remove('is-active');
+    mobileNav.classList.remove('is-active');
+
+    navLinks.forEach(l => l.classList.remove('active'));
+    link.classList.add('active');
+    });
+});
+
+// === FOR SMOOTH SCROLL ==
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
 });
