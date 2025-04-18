3f199ec6d2d07cbf6ea0b3df9bd92410050571c4
Terezie Mazáčová <terezkamazacova@post.cz>
2025-04-18 18:28:50 +0200

    Přidaná menší scroll animace

--- script.js
+++ script.js
@@ -28,3 +28,17 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+
+// == EFFECT ==
+const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+            observer.unobserve(entry.target);
+        }
+    });
+});
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
